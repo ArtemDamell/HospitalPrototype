aec674b99e66ecf77c596361a3e35dafc75d7d00
ArtemDamell <artem.damell@gmail.com>
2023-04-11 13:50:39 +0300

    Add project files.

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField]
+    GameObject EnemyPrefab;
+
+    [SerializeField]
+    int spawnCount = 10;
+
+    [SerializeField]
+    float timeBetweenNextSpawn = 2f;
+
+    GameObject[] SpawnPoints;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get all spawn points
+        SpawnPoints = GameObject.FindGameObjectsWithTag("SpawnPoint");
+    }
+
+    // Create new courutine method
+    /// <summary>
+    /// Spawns a number of enemies at random spawn points, and doubles the spawn count each time the loop ends.
+    /// </summary>
+    /// <returns>
+    /// An IEnumerator object.
+    /// </returns>
+    IEnumerator SpawnZombie()
+    {
+        for (int i = 0; i < spawnCount; i++)
+        {
+            var spawnPoint = Random.Range(0, SpawnPoints.Length); // <-- Get a random number
+
+            Instantiate(EnemyPrefab, SpawnPoints[spawnPoint].transform.position, Quaternion.identity); // <-- Instantiate enemy
+
+            yield return new WaitForSeconds(timeBetweenNextSpawn); // <-- Wait 2 seconds and continue the cycle
+        }
+
+        spawnCount *= 2; // <-- when the loop ends, multiply the counter by 2
+    }
+
+    // Create start courutine public method for another scripts
+    // Call the method when player enter to the Rise trigger
+    /// <summary>
+    /// Starts a coroutine to spawn zombies.
+    /// </summary>
+    public void StartSpawnZombie()
+    {
+        StartCoroutine(nameof(SpawnZombie));
+    }
+}
---
+++ Assets/Scripts/NavMeshAgentEnemy.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshAgentEnemy : MonoBehaviour
+{
+    Camera _target; // <-- Player position
+    NavMeshAgent _agent; // <-- Get nawMesh Agent component
+
+    [SerializeField]
+    float HP = 100; // <-- Enemy HP (100 by default)
+
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        _target = Camera.main;
+
+        // TODO: Remove before release
+        if (_agent != null)
+            print($"{_agent}");
+
+        if (_target != null)
+            print($"Target position {_target.transform.position} finded!");
+        else
+            print($"Target position not found! Please, set tag MainCamera to Player camera.");
+        // ---------------------------
+    }
+
+    void Update()
+    {
+        _agent.SetDestination(_target.transform.position); // <-- Assigning a destination point
+
+        // Check distance between player and enemy
+        if (Vector3.Distance(_agent.transform.position, _target.transform.position) < 0.5f)
+        {
+            
+            // TODO: Fight logic
+        }
+    }
+
+    // Create take damage method
+    void TakeDamage(float _damage)
+    {
+        HP -= _damage;
+
+        print($"Zombie {gameObject.name} take {_damage}. Current lives is {HP}");
+
+        // TODO: Die enemy logic
+        if (HP <= 0)
+        {
+            // Start DIE Courutine
+        }
+    }
+
+    // TODO: DIE Courutine
+}
